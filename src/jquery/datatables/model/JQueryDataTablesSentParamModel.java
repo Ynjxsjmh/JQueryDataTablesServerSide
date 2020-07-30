6adfb0a28e51a051b2b5e5671c41f72f90c89d41
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-07-30 21:06:58 +0800

    Create JQueryDataTable helper object

---
+++ src/jquery/datatables/model/JQueryDataTablesSentParamModel.java
@@ -0,0 +1,43 @@
+package jquery.datatables.model;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JQueryDataTablesSentParamModel {
+
+    /**
+     * Request sequence number sent by DataTable, same value must be returned in
+     * response
+    */
+    private String draw;
+
+    /**
+     * First record that should be shown(used for paging)
+     */
+    private int start;
+
+    /**
+     * Number of records that should be shown in table
+     */
+    private int length;
+
+    /**
+     * Global search criteria.
+     */
+    private JQueryDataTablesSearch search;
+
+    /**
+     * Column's ordering criteria.
+     */
+    private List<JQueryDataTablesOrder> order;
+
+    /**
+     * Table column's list.
+     */
+    private List<JQueryDataTablesColumn> columns;
+
+}
